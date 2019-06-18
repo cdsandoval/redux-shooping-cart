41098950d2f634f3e32491e8d3836066edd60f28
byteconde <iamdeyvi@gmail.com>
2019-06-18 18:30:59 -0500

    Add Product List Preview component

---
+++ src/views/product-list.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Product from '../components/product';
+
+function ProductList() {
+  return <Product productName={'Chocolates Princesa'} price={10} />;
+}
+
+export default ProductList;
