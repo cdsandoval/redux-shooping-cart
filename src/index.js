cc4d1b7204a58944171dd374705f30fb37fc110d
Marieth <mperezzevallos@gmail.com>
2019-06-18 18:30:40 -0500

    Add component productDetail

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import ProductDetail from "./views/product-detail";
+
+ReactDOM.render(<ProductDetail />, document.getElementById("root"));
