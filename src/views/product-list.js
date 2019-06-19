85d89d005cb36e644aabd4b4cc4a7e63500a52c8
byteconde <iamdeyvi@gmail.com>
2019-06-18 20:35:56 -0500

    View tested

--- src/views/product-list.js
+++ src/views/product-list.js
@@ -5,8 +5,8 @@ import { listProduct } from '../data/product-fake';
 function ProductList() {
   return (
     <>
-      {listProduct.map(Product => (
-        <Product productName={Product.name} price={Product.price} />
+      {listProduct.map(p => (
+        <Product productName={p.name} price={p.price} />
       ))}
     </>
   );
