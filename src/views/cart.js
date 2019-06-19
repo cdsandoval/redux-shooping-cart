6217b64b6f661cb5ce9f50ec2e0d53148f260299
Brayan Ciudad <linzeur@gmail.com>
2019-06-18 20:19:18 -0500

    Add app component

---
+++ src/views/cart.js
@@ -0,0 +1,31 @@
+import React from "react";
+import CartItem from "../components/cart-item";
+
+function Cart() {
+  const list = [];
+  let finalTotal = 0;
+  const emptyMessage = <span>You don't have products added</span>;
+  return (
+    <div>
+      <h1>List articles</h1>
+      {list.length === 0 ? (
+        emptyMessage
+      ) : (
+        <ul>
+          <li />
+          {list.map(article => {
+            finalTotal += article.price * article.quantity;
+            return <CartItem />;
+          })}
+        </ul>
+      )}
+      <hr />
+      <section>
+        <span>Total amount:</span>
+        <span>{finalTotal}</span>
+      </section>
+    </div>
+  );
+}
+
+export default Cart;
